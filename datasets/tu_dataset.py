cf71de99d51c6c3b5ece7febcfa6472b34268bd7
Tom Eaton <tomeaton17@gmail.com>
2022-08-22 18:42:47 +0100

    Fixed tu_dataset

--- datasets/tu_dataset.py
+++ datasets/tu_dataset.py
@@ -7,6 +7,7 @@ import torch
 from sklearn.metrics import accuracy_score
 from torch_geometric.data import InMemoryDataset, download_url, extract_zip
 from torch_geometric.io import read_tu_data
+from typing import Callable, List, Optional
 
 
 class TUDataset(InMemoryDataset):
@@ -58,17 +59,28 @@ class TUDataset(InMemoryDataset):
     cleaned_url = ('https://raw.githubusercontent.com/nd7141/'
                    'graph_datasets/master/datasets')
 
-    def __init__(self, root, name, transform=None, pre_transform=None,
-                 pre_filter=None, use_node_attr=False, use_edge_attr=False,
-                 cleaned=False):
+    def __init__(self, root: str, name: str,
+                 transform: Optional[Callable] = None,
+                 pre_transform: Optional[Callable] = None,
+                 pre_filter: Optional[Callable] = None,
+                 use_node_attr: bool = False, use_edge_attr: bool = False,
+                 cleaned: bool = False):
         self.name = name
         self.cleaned = cleaned
         self.num_tasks = 1
         self.task_type = 'classification'
         self.eval_metric = 'accuracy'
-        super(TUDataset, self).__init__(root, transform, pre_transform,
-                                        pre_filter)
-        self.data, self.slices = torch.load(self.processed_paths[0])
+        super().__init__(root, transform, pre_transform, pre_filter)
+
+        out = torch.load(self.processed_paths[0])
+        if not isinstance(out, tuple) and len(out) != 3:
+            raise RuntimeError(
+                "The 'data' object was created by an older version of PyG. "
+                "If this error occurred while loading an already existing "
+                "dataset, remove the 'processed/' directory in the dataset's "
+                "root folder and try again.")
+        self.data, self.slices, self.sizes = out
+
         if self.data.x is not None and not use_node_attr:
             num_node_attributes = self.num_node_attributes
             self.data.x = self.data.x[:, num_node_attributes:]
@@ -77,81 +89,68 @@ class TUDataset(InMemoryDataset):
             self.data.edge_attr = self.data.edge_attr[:, num_edge_attributes:]
 
     @property
-    def raw_dir(self):
-        name = 'raw{}'.format('_cleaned' if self.cleaned else '')
+    def raw_dir(self) -> str:
+        name = f'raw{"_cleaned" if self.cleaned else ""}'
         return osp.join(self.root, self.name, name)
 
     @property
-    def processed_dir(self):
-        name = 'processed{}'.format('_cleaned' if self.cleaned else '')
+    def processed_dir(self) -> str:
+        name = f'processed{"_cleaned" if self.cleaned else ""}'
         return osp.join(self.root, self.name, name)
 
     @property
-    def num_node_labels(self):
-        if self.data.x is None:
-            return 0
-        for i in range(self.data.x.size(1)):
-            x = self.data.x[:, i:]
-            if ((x == 0) | (x == 1)).all() and (x.sum(dim=1) == 1).all():
-                return self.data.x.size(1) - i
-        return 0
+    def num_node_labels(self) -> int:
+        return self.sizes['num_node_labels']
 
     @property
-    def num_node_attributes(self):
-        if self.data.x is None:
-            return 0
-        return self.data.x.size(1) - self.num_node_labels
+    def num_node_attributes(self) -> int:
+        return self.sizes['num_node_attributes']
 
     @property
-    def num_edge_labels(self):
-        if self.data.edge_attr is None:
-            return 0
-        for i in range(self.data.edge_attr.size(1)):
-            if self.data.edge_attr[:, i:].sum() == self.data.edge_attr.size(0):
-                return self.data.edge_attr.size(1) - i
-        return 0
+    def num_edge_labels(self) -> int:
+        return self.sizes['num_edge_labels']
 
     @property
-    def num_edge_attributes(self):
-        if self.data.edge_attr is None:
-            return 0
-        return self.data.edge_attr.size(1) - self.num_edge_labels
+    def num_edge_attributes(self) -> int:
+        return self.sizes['num_edge_attributes']
 
     @property
-    def raw_file_names(self):
+    def raw_file_names(self) -> List[str]:
         names = ['A', 'graph_indicator']
-        return ['{}_{}.txt'.format(self.name, name) for name in names]
+        return [f'{self.name}_{name}.txt' for name in names]
 
     @property
-    def processed_file_names(self):
+    def processed_file_names(self) -> str:
         return 'data.pt'
 
     def download(self):
         url = self.cleaned_url if self.cleaned else self.url
         folder = osp.join(self.root, self.name)
-        path = download_url('{}/{}.zip'.format(url, self.name), folder)
+        path = download_url(f'{url}/{self.name}.zip', folder)
         extract_zip(path, folder)
         os.unlink(path)
         shutil.rmtree(self.raw_dir)
         os.rename(osp.join(folder, self.name), self.raw_dir)
 
     def process(self):
-        self.data, self.slices = read_tu_data(self.raw_dir, self.name)
+        self.data, self.slices, sizes = read_tu_data(self.raw_dir, self.name)
 
-        if self.pre_filter is not None:
+        if self.pre_filter is not None or self.pre_transform is not None:
             data_list = [self.get(idx) for idx in range(len(self))]
-            data_list = [data for data in data_list if self.pre_filter(data)]
+
+            if self.pre_filter is not None:
+                data_list = [d for d in data_list if self.pre_filter(d)]
+
+            if self.pre_transform is not None:
+                data_list = [self.pre_transform(d) for d in data_list]
+
             self.data, self.slices = self.collate(data_list)
+            self._data_list = None  # Reset cache.
 
-        if self.pre_transform is not None:
-            data_list = [self.get(idx) for idx in range(len(self))]
-            data_list = [self.pre_transform(data) for data in data_list]
-            self.data, self.slices = self.collate(data_list)
+        torch.save((self.data, self.slices, sizes), self.processed_paths[0])
 
-        torch.save((self.data, self.slices), self.processed_paths[0])
-
-    def __repr__(self):
-        return '{}({})'.format(self.name, len(self))
+    def __repr__(self) -> str:
+        return f'{self.name}({len(self)})'
 
 class TUEvaluator:
     def __init__(self):
